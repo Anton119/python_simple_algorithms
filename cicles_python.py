da9b5f93e1577e2abd63b28ccfac491e1b2ed5d5
Anton119 <153746663+Anton119@users.noreply.github.com>
2024-02-01 21:04:35 +0300

    Add files via upload
    added more cicles to print triangles and a tree with "*"

--- cicles_python.py
+++ cicles_python.py
@@ -1,6 +1,25 @@
+"""1) Треугольник с катетом у полей
+for i in range(5):
+    for j in range(i):
+        print("*", end=" ")
+    print()    
+print()
+2) Треугольник с гипотенузой от полей
+for i in range(5):
+    for k in range(4-i):
+        print(" ", end=" ")
+    for j in range(i):
+       print("*", end=" ")
+   print()
+3) Ёлка
+print()
 for i in range (5):
+    for j in range(4-i):
+        print(" ",end="")
+    for k in range(i):
+        print("* ",end="")
     print()
-    for j in range (i):
-        print("*",end='')
-       
-        
+################# циклы с while / for 
+"""
+#1) Треугольник с катетом у полей
+    
